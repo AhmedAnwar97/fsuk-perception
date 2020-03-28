2baf64cbab4a41f9ca536f7a4ecc2122d24ec095
Ahmed Anwar <36543394+AhmedAnwar97@users.noreply.github.com>
2020-03-28 16:19:03 +0200

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,176 @@
+from keras.models import Model
+import tensorflow as tf
+from keras import layers
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda, Add, ZeroPadding2D, AveragePooling2D, GlobalMaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.initializers import glorot_uniform
+from keras.layers.merge import concatenate
+from keras.applications.mobilenet import MobileNet
+from keras.applications import InceptionV3
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
+
+import numpy as np
+import math
+
+RESNET50_BACKEND_PATH   = "resnet50_weights_tf_dim_ordering_tf_kernels.h5"    # should be hosted on a server
+
+class BaseFeatureExtractor(object):
+    """docstring for ClassName"""
+
+    # to be defined in each subclass
+    def __init__(self, input_size):
+        raise NotImplementedError("error message")
+
+    # to be defined in each subclass
+    def normalize(self, image):
+        raise NotImplementedError("error message")       
+
+    def get_output_shape(self):
+        return self.feature_extractor.get_output_shape_at(-1)[1:3]
+
+    def extract(self, input_image):
+        return self.feature_extractor(input_image)
+
+
+class ResNet50Feature(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_shape):
+
+        # Define the input as a tensor with shape input_shape
+        self.X_input = Input(shape=(input_shape, input_shape, 3))
+
+    ##### ResNet Basic functions #####
+
+    def identity_block(self, X, f, filters, stage, block):
+        
+        # defining name basis
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+        
+        # Retrieve Filters
+        F1, F2, F3 = filters
+        
+        # Save the input value.
+        X_shortcut = X
+        
+        # First component of main path
+        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+        X = Activation('relu')(X)
+            
+        # Second component of main path
+        X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+        X = Activation('relu')(X)
+
+        # Third component of main path
+        X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+        # Final step: Add shortcut value to main path, and pass it through a RELU activation
+        X = Add()([X, X_shortcut])
+        X = Activation('relu')(X)
+            
+        return X
+
+    def convolutional_block(self, X, f, filters, stage, block, s = 2):
+
+        # defining name basis
+        conv_name_base = 'res' + str(stage) + block + '_branch'
+        bn_name_base = 'bn' + str(stage) + block + '_branch'
+        
+        # Retrieve Filters
+        F1, F2, F3 = filters
+        
+        # Save the input value
+        X_shortcut = X
+
+
+        ##### MAIN PATH #####
+        # First component of main path 
+        X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+        X = Activation('relu')(X)
+        
+        # Second component of main path
+        X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
+        X = Activation('relu')(X)
+
+        # Third component of main path
+        X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
+
+        ##### SHORTCUT PATH ####
+        X_shortcut = Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+        X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
+
+        # Final step: Add shortcut value to main path, and pass it through a RELU activation
+        X = Add()([X, X_shortcut])
+        X = Activation('relu')(X)
+            
+        return X
+
+    def ResNet50(self):
+        # Zero-Padding
+        X = ZeroPadding2D((3, 3))(self.X_input)
+        
+        # Stage 1
+        X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
+        X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
+        X = Activation('relu')(X)
+        X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+        # Stage 2
+        X = self.convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
+        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+        # Stage 3
+        X = self.convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='b')
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='c')
+        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='d')
+
+        # Stage 4
+        X = self.convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+        # Stage 5
+        X = self.convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+        # AVGPOOL
+        X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
+        
+        # output layer
+        # X = Flatten()(X)
+        # X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
+        
+        
+        # Create model
+        # model = Model(inputs = self.X_input, outputs = X, name='ResNet50')
+
+        self.feature_extractor = Model(self.X_input, X)  
+        self.feature_extractor.load_weights(RESNET50_BACKEND_PATH)
+    
+
+    def normalize(self, image):
+        image = image[..., ::-1]
+        image = image.astype('float')
+
+        image[..., 0] -= 103.939
+        image[..., 1] -= 116.779
+        image[..., 2] -= 123.68
+
+        return image 
+
+
