9d13a94b338ce69c2b28851b587a51508a34a778
AhmedAnwar97 <ahmed.elshaf3y4567@gmail.com>
2020-03-30 18:02:12 +0200

    keypoints V1

--- backend.py
+++ backend.py
@@ -23,6 +23,7 @@ WEIGHTS_PATH_NO_TOP = ('https://github.com/fchollet/deep-learning-models/'
                        'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5')
 
 RESNET50_BACKEND_PATH   = "resnet50_weights_tf_dim_ordering_tf_kernels.h5"    # should be hosted on a server
+MOBILENET_BACKEND_PATH  = "mobilenet_backend.h5"                              # should be hosted on a server
 
 class BaseFeatureExtractor(object):
     """docstring for ClassName"""
@@ -41,6 +42,24 @@ class BaseFeatureExtractor(object):
     def extract(self, input_image):
         return self.feature_extractor(input_image)
 
+class MobileNetFeature(BaseFeatureExtractor):
+    """docstring for ClassName"""
+    def __init__(self, input_size):
+        input_image = Input(shape=(input_size, input_size, 3))
+
+        mobilenet = MobileNet(input_shape=(224,224,3), include_top=False)
+        mobilenet.load_weights(MOBILENET_BACKEND_PATH)
+
+        x = mobilenet(input_image)
+
+        self.feature_extractor = Model(input_image, x)  
+
+    def normalize(self, image):
+        image = image / 255.
+        image = image - 0.5
+        image = image * 2.
+
+        return image
 
 class ResNet50Feature(BaseFeatureExtractor):
     """docstring for ClassName"""
@@ -61,133 +80,3 @@ class ResNet50Feature(BaseFeatureExtractor):
         image[..., 2] -= 123.68
 
         return image 
-
-    """
-    def __init__(self, input_shape):
-
-        # Define the input as a tensor with shape input_shape
-        self.X_input = Input(shape=(input_shape, input_shape, 3))
-
-    ##### ResNet Basic functions #####
-
-    def identity_block(self, X, f, filters, stage, block):
-        
-        # defining name basis
-        conv_name_base = 'res' + str(stage) + block + '_branch'
-        bn_name_base = 'bn' + str(stage) + block + '_branch'
-        
-        # Retrieve Filters
-        F1, F2, F3 = filters
-        
-        # Save the input value.
-        X_shortcut = X
-        
-        # First component of main path
-        X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
-        X = Activation('relu')(X)
-            
-        # Second component of main path
-        X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
-        X = Activation('relu')(X)
-
-        # Third component of main path
-        X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
-
-        # Final step: Add shortcut value to main path, and pass it through a RELU activation
-        X = Add()([X, X_shortcut])
-        X = Activation('relu')(X)
-            
-        return X
-
-    def convolutional_block(self, X, f, filters, stage, block, s = 2):
-
-        # defining name basis
-        conv_name_base = 'res' + str(stage) + block + '_branch'
-        bn_name_base = 'bn' + str(stage) + block + '_branch'
-        
-        # Retrieve Filters
-        F1, F2, F3 = filters
-        
-        # Save the input value
-        X_shortcut = X
-
-
-        ##### MAIN PATH #####
-        # First component of main path 
-        X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
-        X = Activation('relu')(X)
-        
-        # Second component of main path
-        X = Conv2D(filters=F2, kernel_size=(f, f), strides=(1, 1), padding='same', name=conv_name_base + '2b', kernel_initializer=glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis=3, name=bn_name_base + '2b')(X)
-        X = Activation('relu')(X)
-
-        # Third component of main path
-        X = Conv2D(filters=F3, kernel_size=(1, 1), strides=(1, 1), padding='valid', name=conv_name_base + '2c', kernel_initializer=glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis=3, name=bn_name_base + '2c')(X)
-
-        ##### SHORTCUT PATH ####
-        X_shortcut = Conv2D(filters=F3, kernel_size=(1, 1), strides=(s, s), padding='valid', name=conv_name_base + '1', kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
-        X_shortcut = BatchNormalization(axis=3, name=bn_name_base + '1')(X_shortcut)
-
-        # Final step: Add shortcut value to main path, and pass it through a RELU activation
-        X = Add()([X, X_shortcut])
-        X = Activation('relu')(X)
-            
-        return X
-
-    def ResNet50(self):
-        # Zero-Padding
-        X = ZeroPadding2D((3, 3))(self.X_input)
-        
-        # Stage 1
-        X = Conv2D(64, (7, 7), strides = (2, 2), name = 'conv1', kernel_initializer = glorot_uniform(seed=0))(X)
-        X = BatchNormalization(axis = 3, name = 'bn_conv1')(X)
-        X = Activation('relu')(X)
-        X = MaxPooling2D((3, 3), strides=(2, 2))(X)
-
-        # Stage 2
-        X = self.convolutional_block(X, f = 3, filters = [64, 64, 256], stage = 2, block='a', s = 1)
-        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='b')
-        X = self.identity_block(X, 3, [64, 64, 256], stage=2, block='c')
-
-        # Stage 3
-        X = self.convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block='a', s=2)
-        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='b')
-        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='c')
-        X = self.identity_block(X, 3, [128, 128, 512], stage=3, block='d')
-
-        # Stage 4
-        X = self.convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
-        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
-        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
-        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
-        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
-        X = self.identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
-
-        # Stage 5
-        X = self.convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
-        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
-        X = self.identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
-
-        # AVGPOOL
-        X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
-        
-        # output layer
-        # X = Flatten()(X)
-        # X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(X)
-
-        self.feature_extractor = Model(self.X_input, X)  
-
-        weights_path = keras_utils.get_file(
-                'resnet50_weights_tf_dim_ordering_tf_kernels.h5',
-                WEIGHTS_PATH,
-                cache_subdir='models',
-                md5_hash='a7b3fe01876f51b976af0dea6bc144eb')
-
-        self.feature_extractor.load_weights(weights_path)
-    """
--- frontend.py
+++ frontend.py
@@ -5,13 +5,16 @@ import tensorflow as tf
 import numpy as np
 import os
 import cv2
+import math
 from utils import decode_netout, compute_overlap, compute_ap
 from keras.applications.mobilenet import MobileNet
 from keras.layers.merge import concatenate
 from keras.optimizers import SGD, Adam, RMSprop
+from keras.initializers import glorot_uniform
 from preprocessing import BatchGenerator
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-from backend import ResNet50Feature
+from backend import ResNet50Feature, MobileNetFeature
+from preprocessing_kp import parse_annotation_kp
 
 class YOLO(object):
     def __init__(self, backend,
@@ -40,8 +43,10 @@ class YOLO(object):
 
         if backend == 'ResNet50':
             self.feature_extractor = ResNet50Feature(self.input_size)
+        elif backend == 'MobileNet':
+            self.feature_extractor = MobileNetFeature(self.input_size)
         else:
-            raise Exception('Architecture not supported! Only support Full Yolo, Tiny Yolo, MobileNet, SqueezeNet, VGG16, ResNet50, and Inception3 at the moment!')
+            raise Exception('Architecture not supported! Only support MobileNet and ResNet50 at the moment!')
 
         print(self.feature_extractor.get_output_shape())    
         self.grid_h, self.grid_w = self.feature_extractor.get_output_shape()        
@@ -460,3 +465,290 @@ class YOLO(object):
 
         return boxes
 
+class keypoints(object):
+    def __init__(self, backend,
+                       input_size, 
+                       labels,
+                       classes=14):
+        
+        self.X_predicted = [None] * 7
+        self.Y_predicted = [None] * 7
+        self.X_groundtruth = [None] * 7
+        self.Y_groundtruth = [None] * 7
+        
+        self.input_size = input_size
+        
+        self.labels   = list(labels)
+        self.nb_class = len(self.labels)
+
+        self.class_wt = np.ones(self.nb_class, dtype='float32')
+
+        ##########################
+        # Make the model
+        ##########################
+
+        # make the feature extractor layers
+        input_image     = Input(shape=(self.input_size, self.input_size, 3))
+        # self.true_boxes = Input(shape=(1, 1, 1, max_box_per_image , 4))  
+
+        if backend == 'ResNet50':
+            self.feature_extractor = ResNet50Feature(self.input_size)
+        else:
+            raise Exception('Architecture not supported! Only support ResNet50 at the moment!')
+
+        print(self.feature_extractor.get_output_shape())    
+        self.grid_h, self.grid_w = self.feature_extractor.get_output_shape()        
+        features = self.feature_extractor.extract(input_image)            
+
+        # make the keypoint detection layer
+
+        output = Flatten()(features)
+        output = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=0))(output)
+
+        self.model = Model([input_image, self.true_boxes], output)
+
+        
+        # initialize the weights of the detection layer
+        layer = self.model.layers[-4]
+        weights = layer.get_weights()
+
+        new_kernel = np.random.normal(size=weights[0].shape)/(self.grid_h*self.grid_w)
+        new_bias   = np.random.normal(size=weights[1].shape)/(self.grid_h*self.grid_w)
+
+        layer.set_weights([new_kernel, new_bias])
+
+        # print a summary of the whole model
+        self.model.summary()
+
+    def delta_(self, p1, p2, x, y):
+        return math.sqrt((x[p1]-x[p1])**2 + (y[p2]-y[p2])**2)
+
+    def cross_ratio(self, surf, x, y):
+        if surf == "left":
+            return (self.delta_(1, 3, x, y)/self.delta_(1, 4, x, y)) / (self.delta_(2, 3, x, y)/self.delta_(2, 4, x, y))
+        elif surf == "right":
+            return (self.delta_(1, 6, x, y)/self.delta_(1, 7, x, y)) / (self.delta_(5, 6, x, y)/self.delta_(5, 7, x, y))
+
+
+    def custom_loss(self, groundtruth, pridection):      # ETH custom loss
+        segma = 0.0001  # Cross ratio controlling factor
+        Cr3D = 1.39408  # The 3D cross ratio of the cone
+        loss = 0
+        for i in range(0, 14, 2):
+            self.X_predicted[i] = pridection[i]
+            self.X_groundtruth[i] = groundtruth[i]
+
+        for j in range(1, 14, 2):
+            self.Y_predicted[j] = pridection[j]
+            self.Y_groundtruth[j] = groundtruth[j]
+
+        for k in range(0,8):
+            loss += (self.X_predicted[k]-self.X_groundtruth[k])**2 + (self.Y_predicted[k]-self.Y_groundtruth[k])**2 + \
+                segma*(self.cross_ratio("left", self.X_predicted, self.Y_predicted)-Cr3D)**2 + segma*(self.cross_ratio("right", self.X_predicted, self.Y_predicted)-Cr3D)**2
+
+        return loss
+
+    def load_weights(self, weight_path):
+        self.model.load_weights(weight_path)
+
+    def train(self, train_imgs,     # the list of images to train the model
+                    valid_imgs,     # the list of images used to validate the model
+                    train_times,    # the number of time to repeat the training set, often used for small datasets
+                    valid_times,    # the number of times to repeat the validation set, often used for small datasets
+                    nb_epochs,      # number of epoches
+                    learning_rate,  # the learning rate
+                    batch_size,     # the size of the batch
+                    warmup_epochs,  # number of initial batches to let the model familiarize with the new dataset
+                    object_scale,
+                    no_object_scale,
+                    coord_scale,
+                    class_scale,
+                    saved_weights_name='best_weights.h5',
+                    debug=False):     
+
+        self.batch_size = batch_size
+
+        self.object_scale    = object_scale
+        self.no_object_scale = no_object_scale
+        self.coord_scale     = coord_scale
+        self.class_scale     = class_scale
+
+        self.debug = debug
+
+        ############################################
+        # Make train generators
+        ############################################  
+
+        X_train ,Y_train =  parse_annotation_kp()
+
+        ############################################
+        # Compile the model
+        ############################################
+
+        sgd = SGD(lr=0.0001, momentum=0.9)  
+        self.model.compile(optimizer = sgd, loss = self.custom_loss, metrics = ['accuracy'])
+
+        ############################################
+        # Make a few callbacks
+        ############################################
+
+        early_stop = EarlyStopping(monitor='val_loss', 
+                           min_delta=0.001, 
+                           patience=3, 
+                           mode='min', 
+                           verbose=1)
+        checkpoint = ModelCheckpoint(saved_weights_name, 
+                                     monitor='val_loss', 
+                                     verbose=1, 
+                                     save_best_only=True, 
+                                     mode='min', 
+                                     period=1)
+        tensorboard = TensorBoard(log_dir=os.path.expanduser('~/logs/'), 
+                                  histogram_freq=0, 
+                                  #write_batch_performance=True,
+                                  write_graph=True, 
+                                  write_images=False)
+
+        ############################################
+        # Start the training process
+        ############################################        
+
+        self.model.fit(X_train, Y_train, epochs = 100, batch_size = 128)      
+
+        ############################################
+        # Compute mAP on the validation set
+        ############################################
+
+        average_precisions = self.evaluate(valid_generator)     
+
+        # print evaluation
+        for label, average_precision in average_precisions.items():
+            print(self.labels[label], '{:.4f}'.format(average_precision))
+        print('mAP: {:.4f}'.format(sum(average_precisions.values()) / len(average_precisions)))
+
+    def evaluate(self, 
+                 generator, 
+                 iou_threshold=0.3,
+                 score_threshold=0.3,
+                 max_detections=100,
+                 save_path=None):
+        """ Evaluate a given dataset using a given model.
+        code originally from https://github.com/fizyr/keras-retinanet
+
+        # Arguments
+            generator       : The generator that represents the dataset to evaluate.
+            model           : The model to evaluate.
+            iou_threshold   : The threshold used to consider when a detection is positive or negative.
+            score_threshold : The score confidence threshold to use for detections.
+            max_detections  : The maximum number of detections to use per image.
+            save_path       : The path to save images with visualized detections to.
+        # Returns
+            A dict mapping class names to mAP scores.
+        """    
+        # gather all detections and annotations
+        all_detections     = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
+        all_annotations    = [[None for i in range(generator.num_classes())] for j in range(generator.size())]
+
+        for i in range(generator.size()):
+            raw_image = generator.load_image(i)
+            raw_height, raw_width, raw_channels = raw_image.shape
+
+            # make the boxes and the labels
+            pred_boxes  = self.predict(raw_image)
+
+            
+            score = np.array([box.score for box in pred_boxes])
+            pred_labels = np.array([box.label for box in pred_boxes])        
+            
+            if len(pred_boxes) > 0:
+                pred_boxes = np.array([[box.xmin*raw_width, box.ymin*raw_height, box.xmax*raw_width, box.ymax*raw_height, box.score] for box in pred_boxes])
+            else:
+                pred_boxes = np.array([[]])  
+            
+            # sort the boxes and the labels according to scores
+            score_sort = np.argsort(-score)
+            pred_labels = pred_labels[score_sort]
+            pred_boxes  = pred_boxes[score_sort]
+            
+            # copy detections to all_detections
+            for label in range(generator.num_classes()):
+                all_detections[i][label] = pred_boxes[pred_labels == label, :]
+                
+            annotations = generator.load_annotation(i)
+            
+            # copy detections to all_annotations
+            for label in range(generator.num_classes()):
+                all_annotations[i][label] = annotations[annotations[:, 4] == label, :4].copy()
+                
+        # compute mAP by comparing all detections and all annotations
+        average_precisions = {}
+        
+        for label in range(generator.num_classes()):
+            false_positives = np.zeros((0,))
+            true_positives  = np.zeros((0,))
+            scores          = np.zeros((0,))
+            num_annotations = 0.0
+
+            for i in range(generator.size()):
+                detections           = all_detections[i][label]
+                annotations          = all_annotations[i][label]
+                num_annotations     += annotations.shape[0]
+                detected_annotations = []
+
+                for d in detections:
+                    scores = np.append(scores, d[4])
+
+                    if annotations.shape[0] == 0:
+                        false_positives = np.append(false_positives, 1)
+                        true_positives  = np.append(true_positives, 0)
+                        continue
+
+                    overlaps            = compute_overlap(np.expand_dims(d, axis=0), annotations)
+                    assigned_annotation = np.argmax(overlaps, axis=1)
+                    max_overlap         = overlaps[0, assigned_annotation]
+
+                    if max_overlap >= iou_threshold and assigned_annotation not in detected_annotations:
+                        false_positives = np.append(false_positives, 0)
+                        true_positives  = np.append(true_positives, 1)
+                        detected_annotations.append(assigned_annotation)
+                    else:
+                        false_positives = np.append(false_positives, 1)
+                        true_positives  = np.append(true_positives, 0)
+
+            # no annotations -> AP for this class is 0 (is this correct?)
+            if num_annotations == 0:
+                average_precisions[label] = 0
+                continue
+
+            # sort by score
+            indices         = np.argsort(-scores)
+            false_positives = false_positives[indices]
+            true_positives  = true_positives[indices]
+
+            # compute false positives and true positives
+            false_positives = np.cumsum(false_positives)
+            true_positives  = np.cumsum(true_positives)
+
+            # compute recall and precision
+            recall    = true_positives / num_annotations
+            precision = true_positives / np.maximum(true_positives + false_positives, np.finfo(np.float64).eps)
+
+            # compute average precision
+            average_precision  = compute_ap(recall, precision)  
+            average_precisions[label] = average_precision
+
+        return average_precisions
+
+    def predict(self, image):
+        image_h, image_w, _ = image.shape
+        image = cv2.resize(image, (self.input_size, self.input_size))
+        image = self.feature_extractor.normalize(image)
+
+        input_image = image[:,:,::-1]
+        input_image = np.expand_dims(input_image, 0)
+        dummy_array = np.zeros((1,1,1,1,self.max_box_per_image,4))
+
+        netout = self.model.predict([input_image, dummy_array])[0]
+        
+
+        return boxes
